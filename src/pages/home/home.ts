6762159443d9136fc55f64b0b55fc826b7cecc13
Alexander Wong <alwongm@gmail.com>
2017-10-08 23:11:32 -0500

    filtered by todo status

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+class Todo {
+  description: string;
+  done: boolean;
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  newTodo: string;
+
+  filteredTodos: Todo[] = [];
+
+  todos: Todo[] = [
+    {
+      description: 'learn ionic',
+      done: false
+    }, {
+      description: 'perform poc',
+      done: true
+    }, {
+      description: 'go to sleep',
+      done: false
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit() {
+    this.showAll();
+  }
+
+  addTodo() {
+    this.todos.push({ description: this.newTodo, done: false});
+    this.newTodo = null;
+  }
+
+  showDone() {
+    this.filteredTodos = this.todos.filter(t => t.done === true);
+  }
+
+  showPending() {
+    this.filteredTodos = this.todos.filter(t => t.done === false);
+  }
+
+  showAll() {
+    this.filteredTodos = this.todos;
   }
 
 }
