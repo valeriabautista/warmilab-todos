e9cdce45bf11df652f203cc3cbc783f7b25a3b7a
pierina kiram <pierinita_4782461@hotmail.com>
2017-11-18 12:12:42 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -5,8 +5,28 @@ import { NavController } from 'ionic-angular';
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+
 export class AboutPage {
-  
+
+  nombre: string = "Valeria bautista";
+  ocupacion: string = "estudiante";
+  edad: number = 14;
+
+
+  datos: any = {
+    nombre: "Valeria Bautista2",
+    ocupacion: "Estudiante2",
+    edad: 14
+  };
+  informacion: any = {
+    origenlbl: "origen3",
+    ciudad:"lima,peru3"
+    };
+
+
+  redsocial: string = "e-mail";
+  correos: string = "valeriabarzola96234380@hotmail.com";
+
   constructor(public navCtrl: NavController) {
 
   }
